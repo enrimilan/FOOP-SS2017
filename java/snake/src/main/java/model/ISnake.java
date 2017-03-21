e9bc278997acd56420f54da4edd0d91690f8317d
Enri Miho <e0929003@student.tuwien.ac.at>
2017-03-21 17:19:35 +0100

    speed at the beginning is 100 setter for speed a snake has a color too

--- java/snake/src/main/java/model/ISnake.java
+++ java/snake/src/main/java/model/ISnake.java
@@ -6,10 +6,17 @@ public interface ISnake {
 
     int getId();
 
+    void setSpeed(int speed);
+
     int getSpeed();
 
     List<IPoint> getPoints();
 
     void setPoints(List<IPoint> points);
+
+    void setColor(String color);
+
+    String getColor();
+
 
 }
--- java/snake/src/main/java/model/impl/Snake.java
+++ java/snake/src/main/java/model/impl/Snake.java
@@ -10,12 +10,18 @@ import java.util.List;
 public class Snake implements ISnake, Serializable {
 
     private int id;
-    private int speed = 1;
+    private int speed = 100;
     private int health; //TODO
+    private String color;
     private List<IPoint> points = new ArrayList<IPoint>();
 
     public Snake(int id) {
         this.id = id;
+    }
+
+    @Override
+    public void setSpeed(int speed) {
+        this.speed = speed;
     }
 
     @Override
@@ -36,6 +42,16 @@ public class Snake implements ISnake, Serializable {
     @Override
     public void setPoints(List<IPoint> points) {
         this.points = points;
+    }
+
+    @Override
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String getColor() {
+        return color;
     }
 
 }
