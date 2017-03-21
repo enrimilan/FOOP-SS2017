f4e7b115bf7980f7dffd39d491b0abc6a31797e8
Enri Miho <e0929003@student.tuwien.ac.at>
2017-03-21 17:09:22 +0100

    override equals

--- java/snake/src/main/java/model/impl/Point.java
+++ java/snake/src/main/java/model/impl/Point.java
@@ -34,4 +34,21 @@ public class Point implements IPoint, Serializable {
         this.y = y;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Point point = (Point) o;
+
+        if (x != point.x) return false;
+        return y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
 }
