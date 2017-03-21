533a968158804d77702c07133d74afd23aba043c
Enri Miho <e0929003@student.tuwien.ac.at>
2017-03-21 17:27:45 +0100

    give a random color to a snake find a free place for a snake and for food too handle eating food (snake grows, it also becomes faster)

--- java/snake/src/main/java/model/impl/Game.java
+++ java/snake/src/main/java/model/impl/Game.java
@@ -1,76 +1,108 @@
 package model.impl;
 
 import model.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.Arrays.asList;
+
 public class Game implements IGame {
 
+    private static final Logger logger = LogManager.getLogger(Game.class);
     private IState state;
+    private List<String> colors = new ArrayList<String>();
 
     public Game(IState state) {
         this.state = state;
+        colors.addAll(asList("blue", "darkred", "darkgreen", "darkorange"));
     }
 
     @Override
     public void addSnake(int id) {
-        //TODO find a free spot for the new snake
         ISnake snake = new Snake(id);
+
+        String color =  colors.remove((int)(Math.random() * (colors.size()-1)));
+        snake.setColor(color);
+
+        IPoint point = state.occupyRandomPoint();
+
         List<IPoint> points = new ArrayList<IPoint>();
-        points.add(new Point(0,5));
-        points.add(new Point(1,5));
-        points.add(new Point(2,5));
-        points.add(new Point(3,5));
+        points.add(point);
         snake.setPoints(points);
         state.getSnakes().put(id, snake);
 
+        logger.debug("Add snake for client {} at position ({},{})", id, point.getX(), point.getY());
+
         // nothing to remove
         state.setToRemove(new ArrayList<IPoint>());
+
+        if(state.getFood() == null) {
+            placeFood();
+        }
     }
 
     @Override
     public void removeSnake(int id) {
         ISnake snake = state.getSnakes().remove(id);
+        colors.add(snake.getColor());
         state.setToRemove(snake.getPoints());
+        state.getAvailablePoints().addAll(snake.getPoints());
     }
 
     @Override
     public IState updateState(int id, Direction direction) {
+
         ISnake snake = state.getSnakes().get(id);
-
         List<IPoint> points = snake.getPoints();
-        List<IPoint> newPoints = new ArrayList<IPoint>();
 
-        ArrayList<IPoint> toRemove = new ArrayList<IPoint>();
-        toRemove.add(points.get(0));
-        state.setToRemove(toRemove);
-
-        for(int i = 0; i< points.size()-1; i++) {
-            newPoints.add(points.get(i+1));
-        }
         IPoint p = points.get(points.size()-1);
-
         if(direction == Direction.RIGHT) {
-            newPoints.add(new Point(p.getX()+1, p.getY()));
+            points.add(new Point(p.getX()+1, p.getY()));
         }
         if(direction == Direction.LEFT) {
-            newPoints.add(new Point(p.getX()-1, p.getY()));
+            points.add(new Point(p.getX()-1, p.getY()));
         }
         if(direction == Direction.UP) {
-            newPoints.add(new Point(p.getX(), p.getY()-1));
+            points.add(new Point(p.getX(), p.getY()-1));
         }
         if(direction == Direction.DOWN) {
-            newPoints.add(new Point(p.getX(), p.getY()+1));
+            points.add(new Point(p.getX(), p.getY()+1));
         }
 
-        snake.setPoints(newPoints);
+        IPoint head = points.get(points.size()-1);
+        ArrayList<IPoint> toRemove = new ArrayList<IPoint>();
+
+        if(head.equals(state.getFood())) {
+            //eat food
+            if(snake.getSpeed() < 800) {
+                snake.setSpeed(snake.getSpeed() + 100);
+            }
+            placeFood();
+            state.getAvailablePoints().remove(head);
+        }
+        else {
+            IPoint tail = points.get(0);
+            points.remove(tail);
+            toRemove.add(tail);
+            state.getAvailablePoints().add(tail);
+        }
+        state.setToRemove(toRemove);
+
         return state;
     }
 
     @Override
     public IState getState() {
         return state;
+    }
+
+    private void placeFood() {
+        IPoint food = state.occupyRandomPoint();
+        logger.debug("Placing food at position ({},{})", food.getX(), food.getY());
+        state.setFood(food);
     }
 
 }
